50736c607cf55251864f356dd42378f2da348bda
xarkamx <albertogmx91@gmail.com>
2019-05-16 01:01:07 -0500

    first commit

---
+++ react-src/src/tools/Ajax.ts
@@ -0,0 +1,33 @@
+import { Helpers } from './Helpers.js';
+export class Ajax extends Helpers {
+  async fetchData(
+    path: string,
+    parameters = {},
+    method = 'get',
+    headers: any = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  ) {
+    parameters = this.objectToSerialize(parameters);
+    const args: any = {
+      headers,
+      body: null,
+      method
+    };
+    if (method.toLowerCase() === 'get') {
+      path += '?' + parameters;
+      parameters = '';
+    } else {
+      args.body = parameters;
+    }
+    args.method = method;
+    console.table(args.body)
+    const data = await fetch(path, args);
+    const result = await data.text();
+    try {
+      return JSON.parse(result);
+    } catch (e) {
+      document.write(result);
+    }
+  }
+}
